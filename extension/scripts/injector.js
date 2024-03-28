18ab94b3b4ee13b099b7dbda666d13fa84529c8d
Keaton Martin <keatonsphone1@gmail.com>
2024-03-27 21:41:33 -0400

    skeleton of injector

---
+++ extension/scripts/injector.js
@@ -0,0 +1,8 @@
+
+// mutation observer for whole document
+const globalObserver = new MutationObserver(mutations => {
+    console.log("test");
+});
+
+// initialize global observer
+globalObserver.observe(document.body, { subtree: true, childList: true, characterData: true, attributes: true });
